23bf30a06dd6ff8f3fcbb5f778bd4489c58a0659
amouna-dev <amouna_1387@yahoo.fr>
2021-10-22 21:03:00 +0100

    second commit

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router()
-const User =require('../models/User')
+const User =require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt =require('jsonwebtoken')
 const isAuth  = require ('../midleware/isAuth')
