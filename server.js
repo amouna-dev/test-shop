23d2cfbd2b837b543214c4130cc2e53b35d9e909
amouna-dev <amouna_1387@yahoo.fr>
2021-10-22 21:26:40 +0100

    second commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if(process.env.NODE_ENV === "production") {
 
     //index.html for all page routes
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     })
 }
 
