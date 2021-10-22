ba99304fac6d9e7bf182a5733848b53240173533
amouna-dev <amouna_1387@yahoo.fr>
2021-10-22 21:09:28 +0100

    second commit

--- midleware/isAuth.js
+++ midleware/isAuth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const User = require('../models/user')
 require('dotenv').config({path:'../config/.env'})
 
 const isAuth = async(req,res,next)=>{
