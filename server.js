52be45d4bec987123a4df3389dc5e1a547506a09
amouna-dev <84313951+amouna-dev@users.noreply.github.com>
2021-10-27 21:36:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === "production") {
 
     //affich index.html for all page routes not exist
     app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "build", "index.html"))
     })
 }
 
